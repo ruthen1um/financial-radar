7afb0e199990396511fa22c63a41859213f4cf92
Skillet323 <mr.borgachev@gmail.com>
2025-10-19 14:55:55 +0300

    ISO-8601 and dependencies

---
+++ src/time_parse.h
@@ -0,0 +1,16 @@
+﻿#ifndef TIME_PARSER_H
+#define TIME_PARSER_H
+
+#include "date/date.h"
+#include <chrono>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+using namespace std::chrono;
+using namespace date;
+
+sys_time<milliseconds>
+parse8601_full(std::string const& input);
+
+#endif
